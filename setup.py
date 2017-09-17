7561a8f12801a32685f5022c38129d6021e51863
AJ Keller <pushtheworldllc@gmail.com>
2017-09-17 17:24:13 -0400

    FIX: setup.py to use setup tools

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from distutils.core import setup
-setup(
-  name = 'RaspberryPiADS1299',
-  packages = ['RaspberryPiADS1299'], # this must be the same as the name above
-  version = '0.1.0',
-  description = 'A lib for running ADS1299 on Rasberry Pi',
-  author = 'Fredric Simard',
-  author_email = 'frederic.simard.1@outlook.com',
-  url = 'https://github.com/PushTheWorld/RaspberryPiADS1299', # use the URL to the github repo
-  download_url = 'https://github.com/PushTheWorld/RaspberryPiADS1299/archive/0.1.0.tar.gz', # I'll explain this in a second
-  keywords = ['device', 'control', 'eeg', 'emg', 'ekg', 'ads1299', 'raspberry', 'pi'], # arbitrary keywords
-  classifiers = []
-)
+from setuptools import setup, find_packages
+
+setup(name = 'RaspberryPiADS1299',
+      packages = ['RaspberryPiADS1299'], # this must be the same as the name above
+      version = '0.1.1',
+      description = 'A lib for running ADS1299 on Rasberry Pi',
+      author='Fredric Simard',
+      author_email='frederic.simard.1@outlook.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['numpy'],
+      url='https://github.com/PushTheWorld/RaspberryPiADS1299',  # use the URL to the github repo
+      download_url='https://github.com/PushTheWorld/RaspberryPiADS1299/archive/0.1.0.tar.gz',
+      keywords=['device', 'control', 'eeg', 'emg', 'ekg', 'ads1299', 'raspberry', 'pi'],  # arbitrary keywords
+      zip_safe=False)
